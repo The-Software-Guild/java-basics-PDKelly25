f1fdde5bacdf28aa21aa8b9a571933a238ed8711
Patrick <pdkelly25@gmail.com>
2022-04-22 11:05:43 +0100

    001: first 2 exercises complete

---
+++ src/main/java/com/m3/c216/assessment/DogGenetics.java
@@ -0,0 +1,27 @@
+package com.m3.c216.assessment;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class DogGenetics {
+    private static int limit=101; //100%
+
+    public int randomiser(int n){
+        Random r = new Random();
+        int percentage = r.nextInt((n));
+        limit -= percentage; //decreases every time function called.
+        return percentage;
+    }
+
+    public static void main(String[] args) {
+        DogGenetics d = new DogGenetics(); // create object to call randomiser method non-statically.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your dog's name?");
+        String name = scanner.nextLine();
+        System.out.printf("%s's Esquire is: %n" +
+                "%n%d St. Bernard %n%d Chihuahua %n%d Dramatic RedNosed Asian Pug %n%d Common Cur %n%d King Doberman%n" +
+                        "%nWow! That's quite the dog!",
+                name, d.randomiser(limit), d.randomiser(limit), d.randomiser(limit), d.randomiser(limit), d.randomiser(limit));
+                      //calls function which returns the percentage instead of storing it in variables.
+    }
+}
---
+++ src/main/java/com/m3/c216/assessment/RockPaperScissors.java
@@ -0,0 +1,59 @@
+package com.m3.c216.assessment;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockPaperScissors {
+    private int rounds;
+    private static int tie=0;
+    private static int win=0;
+    private static int lose=0;
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public void play(){
+        Scanner game_input = new Scanner(System.in);
+        System.out.println("Rock ('1'), Paper ('2'), or Scissors ('3')?");
+        int user_hand = game_input.nextInt();
+        Random r = new Random();
+        int comp_hand = r.nextInt(3);
+        if (comp_hand == user_hand){
+            System.out.println("Tie!");
+            tie++;
+        }
+        else if ((comp_hand == 1 && user_hand==2) || (comp_hand == 2 && user_hand==3) || (comp_hand == 3 && user_hand==1)){
+            System.out.println("You Win!");
+            win++;
+        }
+        else {
+            System.out.println("You lose!");
+            lose++;
+        }
+    }
+
+    public static void main(String[] args) {
+        String play_again = "y";
+        Scanner scanner = new Scanner(System.in);
+        while (play_again.equals("y")) {
+            System.out.println("Enter the number of rounds you want to play between 1 and 10, inclusive.");
+            int input = scanner.nextInt();
+            if (input < 1 || input > 10) {
+                System.out.println("Error - please enter a number between 1 and 10, inclusive.");
+                break;
+            } else {
+                RockPaperScissors game = new RockPaperScissors();
+                game.setRounds(input); // setter method
+                for (int x = 0; x < input; x++) {
+                    game.play();
+                }
+                String winner = (win>lose) ? "User! Congratulations!" : "Computer! Better luck next time.";
+                System.out.printf("User Wins: %d, Computer Wins: %d, Ties %d %nOverall Winner: %s", win, lose, tie, winner);
+                System.out.println("\ndo you want to play again? 'y' - yes, 'n' - no");
+                play_again = scanner.next();
+            }
+        }
+
+    }
+}
